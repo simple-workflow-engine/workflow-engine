415374dd909e7bf239af7ce473448ea234546535
Nisarg Bhatt <nisargrbhatt@gmail.com>
2023-10-14 18:37:34 +0530

    isolated-vm to vm

--- src/engine/processor/function.ts
+++ src/engine/processor/function.ts
@@ -1,6 +1,8 @@
 import type { Task } from '../../engine/tasks/index';
 import { safeAsync } from '@lib/utils/safe';
-import ism from 'isolated-vm';
+import { performance } from 'node:perf_hooks';
+import vm from 'vm';
+
 import { LogSeverity, type WorkflowLogger } from '../logger';
 import axios from 'axios';
 import { ProcessorProcess } from '../engine.interface';
@@ -19,78 +21,69 @@ export class FunctionProcessor implements ProcessorProcess {
     task: Task,
   ): Promise<{
     response: unknown;
+    /**
+     * In Seconds
+     */
+    timeTaken: number;
+    /**
+     * In MB
+     */
+    memoryUsage: number;
   }> {
-    const ismObj = new ism.Isolate();
-
-    const context = await ismObj.createContext();
-
-    const jail = context.global;
-
-    await Promise.all([
-      context.evalClosureSync(
-        `
-        globalThis.console = {
-          log: $0,
-          info: $1,
-          warn: $2,
-          error: $3
-        }
-      `,
-        [
-          (...args: any[]) => loggerObj.log(LogSeverity.log, ...args),
-          (...args: any[]) => loggerObj.log(LogSeverity.info, ...args),
-          (...args: any[]) => loggerObj.log(LogSeverity.warn, ...args),
-          (...args: any[]) => loggerObj.log(LogSeverity.error, ...args),
-        ],
-      ),
-      context.evalClosure(
-        `
-          {
-            axios = function (...args) {
-                return $0.apply(undefined, args, { arguments: { copy: true }, result: { promise: true, copy: true } });
-            };
-          }
-        `,
-        [httpClient],
-        { arguments: { reference: true } },
-      ),
-      jail.set('global', jail.derefInto()),
-      jail.set('workflowParams', params, {
-        copy: true,
-      }),
-      jail.set('workflowGlobal', global, {
-        copy: true,
-      }),
-      jail.set('workflowResults', results, {
-        copy: true,
-      }),
-    ]);
+    const tick = performance.now();
+    const context = vm.createContext({
+      console: {
+        log: (...args: any[]) => loggerObj.log(LogSeverity.log, ...args),
+        info: (...args: any[]) => loggerObj.log(LogSeverity.info, ...args),
+        warn: (...args: any[]) => loggerObj.log(LogSeverity.warn, ...args),
+        error: (...args: any[]) => loggerObj.log(LogSeverity.error, ...args),
+      },
+      axios: httpClient,
+      workflowParams: params,
+      workflowGlobal: global,
+      workflowResults: results,
+    });
 
     const evalResult = await safeAsync(
-      await context.eval(
+      await vm.runInNewContext(
         `
     ${task.exec}
     handler();
     `,
-        {
-          promise: true,
-          copy: true,
-        },
+        context,
+        {},
       ),
     );
-    ismObj.dispose();
 
     if (evalResult.success === false) {
       throw evalResult.error;
     }
 
+    const memoryResult = await safeAsync(
+      vm.measureMemory({ mode: 'summary', context: context }).then((result) => {
+        console.log(context?.consts);
+        return result.total?.jsMemoryEstimate;
+      }),
+    );
+
+    const memoryUsage =
+      (memoryResult.success ? memoryResult.data ?? 0 : 0) / 1024 / 1024;
+
+    const tock = performance.now();
+
+    const timeTaken = (tock - tick) / 1000;
+
     if (!evalResult.data) {
       return {
         response: {},
+        memoryUsage,
+        timeTaken,
       };
     }
     return {
       response: evalResult.data,
+      memoryUsage,
+      timeTaken,
     };
   }
 }
--- src/engine/processor/guard.ts
+++ src/engine/processor/guard.ts
@@ -1,6 +1,7 @@
 import { safeAsync } from '@lib/utils/safe';
 import type { Task } from '../tasks';
-import ism from 'isolated-vm';
+import { performance } from 'node:perf_hooks';
+import vm from 'vm';
 import axios from 'axios';
 import { LogSeverity, type WorkflowLogger } from '../logger';
 import { ProcessorProcess } from '../engine.interface';
@@ -19,73 +20,62 @@ export class GuardProcessor implements ProcessorProcess {
     task: Task,
   ): Promise<{
     response: boolean;
+    /**
+     * In Seconds
+     */
+    timeTaken: number;
+    /**
+     * In MB
+     */
+    memoryUsage: number;
   }> {
-    const ismObj = new ism.Isolate();
-
-    const context = await ismObj.createContext();
-
-    const jail = context.global;
-
-    await Promise.all([
-      context.evalClosureSync(
-        `
-        globalThis.console = {
-          log: $0,
-          info: $1,
-          warn: $2,
-          error: $3
-        }
-      `,
-        [
-          (...args: any[]) => loggerObj.log(LogSeverity.log, ...args),
-          (...args: any[]) => loggerObj.log(LogSeverity.info, ...args),
-          (...args: any[]) => loggerObj.log(LogSeverity.warn, ...args),
-          (...args: any[]) => loggerObj.log(LogSeverity.error, ...args),
-        ],
-      ),
-      context.evalClosure(
-        `
-          {
-            axios = function (...args) {
-                return $0.apply(undefined, args, { arguments: { copy: true }, result: { promise: true, copy: true } });
-            };
-          }
-        `,
-        [httpClient],
-        { arguments: { reference: true } },
-      ),
-      jail.set('global', jail.derefInto()),
-      jail.set('workflowParams', params, {
-        copy: true,
-      }),
-      jail.set('workflowGlobal', global, {
-        copy: true,
-      }),
-      jail.set('workflowResults', results, {
-        copy: true,
-      }),
-    ]);
+    const tick = performance.now();
+    const context = vm.createContext({
+      console: {
+        log: (...args: any[]) => loggerObj.log(LogSeverity.log, ...args),
+        info: (...args: any[]) => loggerObj.log(LogSeverity.info, ...args),
+        warn: (...args: any[]) => loggerObj.log(LogSeverity.warn, ...args),
+        error: (...args: any[]) => loggerObj.log(LogSeverity.error, ...args),
+      },
+      axios: httpClient,
+      workflowParams: params,
+      workflowGlobal: global,
+      workflowResults: results,
+    });
 
     const evalResult = await safeAsync(
-      await context.eval(
+      await vm.runInNewContext(
         `
     ${task.exec}
     handler();
     `,
-        {
-          promise: true,
-          copy: true,
-        },
+        context,
+        {},
       ),
     );
-    ismObj.dispose();
 
     if (evalResult.success === false) {
       throw evalResult.error;
     }
 
+    const memoryResult = await safeAsync(
+      vm.measureMemory({ mode: 'summary', context: context }).then((result) => {
+        console.log(context?.consts);
+        return result.total?.jsMemoryEstimate;
+      }),
+    );
+
+    const memoryUsage =
+      (memoryResult.success ? memoryResult.data ?? 0 : 0) / 1024 / 1024;
+
+    const tock = performance.now();
+
+    const timeTaken = (tock - tick) / 1000;
+
     return {
       response: Boolean(evalResult.data),
+      memoryUsage,
+      timeTaken,
     };
   }
 }
